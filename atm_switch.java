52baa77d3f2cdf4897944f50bcecd294821f9637
KarakusAlican <tr.alikarakus2008@gmail.com>
2024-06-11 17:00:08 +0300

    neww

--- atm_switch.java
+++ atm_switch.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
-public class atm_proje {
-    public static void main(String[]args) {
-
-        Scanner scanner=new Scanner(System.in);
+public class atm_switch {
+    public static void main(String[]args){
+        Scanner scanner =new Scanner(System.in);
         int bakiye =1000;
         int çekme=0;
         int yatırma=0;
@@ -15,42 +14,34 @@ public class atm_proje {
         System.out.println("*****************************");
         System.out.println(işlemler);
         System.out.println("*****************************");
+        String islem = scanner.nextLine();
 
-
-        while (true) {
-            System.out.println("işlemi seçiniz :");
-            String işlem=scanner.nextLine();
-            if (işlem =="a"){
+        switch (islem){
+            case "a":
                 System.out.println(bakiye);
-                break;
-            }
-            if (işlem.equals("b")){
 
+            case "b":
                 System.out.println("lütfen çekmek istediğiniz miktarı giriniz :");
                 int m1 =scanner.nextInt();
                 int s1 =bakiye-m1;
                 System.out.println("kalan bakiye :" +s1);
                 break;
 
-
-            }
-
-            if (işlem=="c") {
-
+            case "c":
                 System.out.println("lütfen yatırmak istediğiniz miktarı giriniz :");
                 int m2 = scanner.nextInt();
                 int s2 =bakiye +m2;
                 System.out.println("yeni bakiye :" +s2);
                 break;
-            }
-            if (işlem=="q"){
-                break;
-            }
+
+            case "q":
+                System.out.println("Çikis yapiliyor...");
                 break;
 
+            default:
+                System.out.println("burdayiz.");
 
         }
 
     }
-
 }
